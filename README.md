c0237ee33baa5a80b54407d976efe76d36c9c2cc
Landon Smith <creeperthemighty@gmail.com>
2024-09-21 16:48:37 -0600

    Fixed up the README section

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CSE310_Projects
 Projects made for CSE310
+
+Part One:
+
+#A short program that introduces the beginning of this classes projects
+
+#This project is made to print a simple phrase, being "Hello World!"
+
+#[Software Demo Video]https://www.loom.com/share/3e8024f18c684711b0863e70cbbed725?sid=46f84284-ff2f-424d-826f-5cfc3ae6c43c
+
+# Development Environment
+
+For this project, I just used VSCode as my IDE and not much else really, given this is something I did in about two lines. 
+
+Python is a highly versatile language that is used in almost every field. As you can see here, this project is no exception
+
+# Useful Websites
+N/A or Past Experience
+
--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,2 @@
 v = print("Hello World!")
 print(v)
-
-#{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
-
-#A short program that introduces the beginning of this classes projects
-
-#This project is made to print a simple phrase
-
-#{Provide a link to your YouTube demonstration.  It should be a one minute demo of the software running and a walkthrough of the code.}
-
-#[Software Demo Video](http://youtube.link.goes.here)
-
-# Development Environment
-
-#{Describe the tools that you used to develop the software}
-
-#{Describe the programming language that you used}
-
-# Useful Websites
-
